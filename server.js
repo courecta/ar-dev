488981f20827c9cfb566e5b960c0dd62b960f7a0
courecta <41247044S@ntnu.edu.tw>
2024-04-19 17:00:45 +0800

    added index.html and server

---
+++ server.js
@@ -0,0 +1,15 @@
+const { createServer } = require('node:http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('pinged!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
